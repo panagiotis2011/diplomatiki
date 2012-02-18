acab3196bfa3f152c1469fd3bc39c8bb9784dbd9
panagiotis2011 <sch@sch.gr>
2012-02-18 21:28:24 +0200

    ok

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class Students::RegistrationsController < Devise::RegistrationsController
   before_filter :get_lessons
 
@@ -10,6 +11,17 @@ class Students::RegistrationsController < Devise::RegistrationsController
   super
 end
 
+def create
+     if verify_recaptcha
+       super
+     else
+       build_resource
+       clean_up_passwords(resource)
+       flash.now[:error] = "Υπάρχει ένα σφάλμα με τον κωδικό recaptcha. Παρακαλώ ξαναπροσπαθήσε. [Κωδικός σφάλαμτος: " + flash[:recaptcha_error] + "]"
+       flash.delete(:recaptcha_error)
+       render_with_scope :new
+     end
+   end
 private
   def get_lessons
     @all_lessons = Lesson.all
