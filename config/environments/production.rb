5d434c3e2b8257190cb632ad5ea15890fa97ccc5
panagiotis2011 <sch@sch.gr>
2012-02-26 02:10:12 +0200

    okkk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,14 +58,8 @@ Diplomatiki::Application.configure do
   config.active_support.deprecation = :notify
 
   Rails.application.config.middleware.use OmniAuth::Builder do
-require 'openid/store/filesystem'
 
-  provider :twitter, 'HWeGxTH2ysMmPnQUhbsQg', 'ud2YQ1u9PByzQsfGE2OF2khrC3vR4RIw6Oxpju5b948', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  provider :facebook, '303767306344343', '11193324352b3c009165ca20030e3ae2'
-  provider :github, 'CLIENT ID', 'SECRET', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  provider :openid, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
-  provider :openid, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  provider :openid, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
-
-end
+    provider :twitter, 'HWeGxTH2ysMmPnQUhbsQg', 'ud2YQ1u9PByzQsfGE2OF2khrC3vR4RIw6Oxpju5b948', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+    provider :facebook, '303767306344343', '11193324352b3c009165ca20030e3ae2'
+  end
 end
