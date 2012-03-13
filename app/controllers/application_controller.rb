7b4d2eeb9d9dde02ce6f8c8c85ec65f597d83ec7
panagiotis2011 <sch@sch.gr>
2012-03-13 23:08:39 +0200

    ok now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_student!, :except => [:index, :all, :show, :about]
   before_filter :count_articles
-  before_filter :mailer_set_url_options
+
 
   def about
   end
