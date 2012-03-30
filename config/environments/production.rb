545e47f3a9ec19c5c6c4c49245357827fd1cb994
panagiotis2011 <sch@sch.gr>
2012-03-31 02:27:29 +0300

    test3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Eap2::Application.configure do
+Diplomatiki::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
