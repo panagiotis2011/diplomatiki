4f5ca71b0582a9c5d2bc4a63400522c919c433b2
panagiotis2011 <sch@sch.gr>
2012-06-01 22:34:00 +0300

    01/06/2012

--- db/migrate/20120413204448_remove_fb_student_uid_from_student.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFbStudentUidFromStudent < ActiveRecord::Migration
-  #def self.up
-    #remove_column :students, :fb_student_uid
-  #end
-
-  #def self.down
-    #add_column :students, :fb_student_uid, :integer
-  #end
-end
